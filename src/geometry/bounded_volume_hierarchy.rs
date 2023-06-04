3563f8402808f03d043fed2cbdf6daaf3ec3a853
Nicholas Hansen <ndhansen@leskat.net>
2023-06-04 17:09:14 +0100

    Added some bvh stuff

--- src/geometry/bounded_volume_hierarchy.rs
+++ src/geometry/bounded_volume_hierarchy.rs
@@ -3,54 +3,60 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 use crate::scene::{
-    bounding_box::AxisAlignedBoundingBox,
-    hit_record::HitRecord,
-    hittable::{self, Hittable},
+    bounding_box::AxisAlignedBoundingBox, hit_record::HitRecord, hittable::Hittable,
 };
 
 use super::ray::Ray;
 
-pub struct BvhNode {
-    left: Box<dyn Hittable>,
-    right: Box<dyn Hittable>,
+pub struct BvhNode<'a> {
+    left: &'a Box<dyn Hittable>,
+    right: &'a Box<dyn Hittable>,
     bounding_box: AxisAlignedBoundingBox,
 }
 
-impl BvhNode {
+impl<'a> BvhNode<'a> {
     pub fn new(
-        src_objects: &Vec<Box<dyn Hittable>>,
+        src_objects: &'a Vec<Box<dyn Hittable>>,
         start: usize,
         end: usize,
         time_0: f64,
         time_1: f64,
-    ) -> BvhNode {
+    ) -> BvhNode<'a> {
         let mut generator = rand::thread_rng();
         let axis = generator.gen_range(0..2);
         let object_span = end - start;
 
-        let (left, right) = if object_span == 1 {
-            (src_objects[start], src_objects[start])
+        let (left, right): (&Box<dyn Hittable>, &Box<dyn Hittable>) = if object_span == 1 {
+            (&src_objects[start], &src_objects[start])
         } else if object_span == 2 {
-            if BvhNode::box_compare(src_objects[start], src_objects[start + 1], axis) {
-                (src_objects[start], src_objects[start + 1])
+            let compare_results =
+                BvhNode::box_compare(&src_objects[start], &src_objects[start + 1], axis);
+            if compare_results.is_lt() {
+                (&src_objects[start], &src_objects[start + 1])
             } else {
-                (src_objects[start + 1], src_objects[start])
+                (&src_objects[start + 1], &src_objects[start])
             }
         } else {
             src_objects.sort_by(|left, right| BvhNode::box_compare(left, right, axis));
 
             let mid = start + object_span / 2;
             (
-                Box::new(BvhNode::new(src_objects, start, mid, time_0, time_1)),
-                Box::new(BvhNode::new(src_objects, mid, end, time_0, time_1)),
+                &Box::new(BvhNode::new(src_objects, start, mid, time_0, time_1)),
+                &Box::new(BvhNode::new(src_objects, mid, end, time_0, time_1)),
             )
         };
 
-        let left_box = left.bounding_box(time_0, time_1);
-        let right_box = right.bounding_box(time_0, time_1);
+        let left_box = left.bounding_box(time_0, time_1).unwrap();
+        let right_box = right.bounding_box(time_0, time_1).unwrap();
+        let surrounding_box = AxisAlignedBoundingBox::surrounding_box(&left_box, &right_box);
+        BvhNode {
+            left,
+            right,
+            bounding_box: surrounding_box,
+        }
     }
 
-    fn box_compare(a: Box<dyn Hittable>, b: Box<dyn Hittable>, axis: usize) -> Ordering {
+    fn box_compare(a: &Box<dyn Hittable>, b: &Box<dyn Hittable>, axis: usize) -> Ordering {
         let left_box = a.bounding_box(0.0, 0.0).unwrap();
         let right_box = b.bounding_box(0.0, 0.0).unwrap();
 
@@ -63,7 +69,7 @@ impl BvhNode {
     }
 }
 
-impl Hittable for BvhNode {
+impl Hittable for BvhNode<'_> {
     fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
         if !self.bounding_box.hit(ray, t_min, t_max) {
             return None;
